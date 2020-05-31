11357d268126fd488aafd37dad63e08f96033154
Matthew <matthewl@poshmark.com>
2020-05-30 17:51:00 -0700

    Adding webpack configurations and setting up express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static(__dirname + '/../client/dist'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+// kill $(lsof -t -i:3001)
